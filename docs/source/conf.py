1d6f5d5232aaf626656cf43dbad7f2acc0ce9452
Andrej Mäsiar <86407432+aurichalcite@users.noreply.github.com>
2025-09-12 02:22:45 -0700

    refactor: Use consistent double quotes in Sphinx config file
    - Replaced single quotes with double quotes in `conf.py` for consistency with project style.
    - Adjusted inline comments for better alignment.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Ymago'
-copyright = '2025, Andrej Mäsiar'
-author = 'Andrej Mäsiar'
-release = '0.1.0'
+project = "Ymago"
+copyright = "2025, Andrej Mäsiar"
+author = "Andrej Mäsiar"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -18,15 +18,14 @@ extensions = [
     "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
-    "sphinx.ext.napoleon",          # Google/NumPy docstrings
-    "sphinx_autodoc_typehints",     # Nice type hints in docs
-    "sphinxcontrib.mermaid",        # Optional diagrams
+    "sphinx.ext.napoleon",  # Google/NumPy docstrings
+    "sphinx_autodoc_typehints",  # Nice type hints in docs
+    "sphinxcontrib.mermaid",  # Optional diagrams
 ]
 
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -48,4 +47,4 @@ myst_enable_extensions = [
     "colon_fence",
 ]
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
